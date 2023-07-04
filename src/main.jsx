c045fd53e72e780840af5a9b7c66632f6bbb0df2
Alvaro Barba <abarba.lod@gmail.com>
2023-07-04 11:58:12 +0200

    project folder structure and styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+// Importar assets (recursos: hojas de estilos, imagenes, fuetes)
+import './assets/fonts/fontawesome-free-6.1.2-web/css/all.css'
+import './assets/css/normalize.css'
+import './assets/css/styles.css'
+import './assets/css/responsive.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
